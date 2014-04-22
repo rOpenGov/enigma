39061f939c1c52c5f56f11ece766c2247d9f9796
Scott Chamberlain <myrmecocystus@gmail.com>
2014-04-22 07:19:18 -0700

    added metadata fxn, and cleaned up rd files

--- R/enigma_data.r
+++ R/enigma_data.r
@@ -1,6 +1,6 @@
 #' Fetch a dataset from Enigma.
 #' 
-#' @param dataset Dataset name. See the `\code{\link{enigma_}} function 
+#' @param dataset Dataset name. Required.
 #' @param limit (numeric) Number of rows of the dataset to return. Default: 50
 #' @param select (character) Vector of columns to be returned with each row. Default is to return 
 #' all columns.
@@ -9,14 +9,14 @@
 #' @param page (numeric) Paginate row results and return the nth page of results. Pages are 
 #' calculated based on the current limit, which defaults to 50.
 #' @param where (character) Filter results with a SQL-style "where" clause. Only applies to 
-#' numerical columns – use the \code{search} parameter for strings. Valid operators are >, < and =. 
+#' numerical columns - use the \code{search} parameter for strings. Valid operators are >, < and =. 
 #' Only one \code{where} clause per request is currently supported.
 #' @param search (character) Filter results by only returning rows that match a search query. By 
 #' default this searches the entire table for matching text. To search particular fields only, use 
 #' the query format "@@fieldname query". To match multiple queries, the | (or) operator can be used 
 #' eg. "query1|query2".
 #' @param key (character) An Enigma API key. Supply in the function call, or store in your
-#' \code{.Rprofile} file, or do \code{options(enigmaKey = "<your key>")}
+#' \code{.Rprofile} file, or do \code{options(enigmaKey = "<your key>")}. Required.
 #' @param curlopts (list) Curl options passed on to \code{httr::GET}
 #' @import httr RJSONIO assertthat 
 #' @importFrom plyr rbind.fill
---
+++ R/enigma_metadata.r
@@ -0,0 +1,80 @@
+#' Search for metadata on a dataset from Enigma.
+#' 
+#' @param dataset Dataset name. Required.
+#' @param key (character) An Enigma API key. Supply in the function call, or store in your
+#' \code{.Rprofile} file, or do \code{options(enigmaKey = "<your key>")}
+#' @param curlopts (list) Curl options passed on to \code{httr::GET}
+#' @import httr RJSONIO assertthat 
+#' @importFrom plyr rbind.fill
+#' @export
+#' @details Notice when you run the examples that the format of output is different for the 
+#' "parent nodes" vs. the "table nodes". Where the parent nodes have ouput$meta slots for 
+#' paths, immediate nodes and children tables, while the table nodes have ouput$meta slots for 
+#' info, table, ancestor datapaths, database boundary datapath, database boundary label, database 
+#' boundary tables, and paths, and an additional slot for description of table column attributes. 
+#' @examples \dontrun{ 
+#' # Parent node response attributes
+#' enigma_metadata(dataset='us.gov.whitehouse')
+#' enigma_metadata(dataset='us.gov.dot.rita.trans-stats.air-carrier-statistics')
+#' 
+#' # Table node response attributes
+#' ## US white house visitor list
+#' enigma_metadata(dataset='us.gov.whitehouse.visitor-list')
+#' 
+#' ## Domestic Market Flight Statistics (Final Destination)
+#' enigma_metadata(dataset='us.gov.dot.rita.trans-stats.air-carrier-statistics.t100d-market-all-carrier')
+#' }
+
+enigma_metadata <- function(dataset=NULL, key=NULL, curlopts=list())
+{
+  if(is.null(key))
+    key <- getOption("enigmaKey", stop("need an API key for PLoS Journals"))
+
+  url <- 'https://api.enigma.io/v2/meta/%s/%s'
+  url <- sprintf(url, key, dataset)
+  res <- GET(url, query=list(), curlopts)
+  stop_for_status(res)
+  assert_that(res$headers$`content-type` == 'application/json; charset=utf-8')
+  dat <- content(res, as = "text", encoding = 'utf-8')
+  json <- fromJSON(dat)
+  success <- json$success
+  meta <- process_meta(json)
+  result_names <- names(json$result)
+  if(any(result_names %in% "columns")){
+    out <- list(success = success, meta = meta, columns = process_cols(json))
+  } else {
+    out <- list(success = success, meta = meta)
+  }
+  class(out) <- "enigma_meta"
+  return( out )
+}
+
+process_meta <- function(x){
+  result_names <- names(x$result)
+  if(!any(result_names %in% "columns")){
+    list(paths=lapply(x$result$path, as.list),
+         immediate_nodes=lapply(x$result$immediate_nodes, as.list),
+         children_tables=lapply(x$result$children_tables, as.list))
+  } else {
+    tmp <- x$result$metadata
+    tablemeta <- sapply(tmp, "[[", "value")
+    names(tablemeta) <- c('total_rows','last_updated') 
+    list(info=as.list(x$info), 
+         table=as.list(tablemeta), 
+         ancestor_datapaths=x$result$ancestor_datapaths,
+         db_boundary_datapath=x$result$db_boundary_datapath,
+         db_boundary_label=x$result$db_boundary_label,
+         db_boundary_tables=lapply(x$result$db_boundary_tables, as.list),
+         paths=lapply(x$result$path, as.list))
+  }
+}
+
+process_cols <- function(x){
+  columns <- x$result$columns
+  colsdat <- do.call(rbind.fill, 
+                     lapply(columns, function(x){ 
+                       x[sapply(x, is.null)] <- NA; data.frame(x, stringsAsFactors = FALSE) 
+                     }))  
+  colsdat_table <- data.frame(id=colsdat$id,label=colsdat$label,type=colsdat$type,index=colsdat$index)
+  list(table=colsdat_table, description=as.list(colsdat$description))
+}
