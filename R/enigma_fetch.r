fc585e514a3332d32c7ba62ba6f12ec2a8f175f3
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-07 13:32:50 -0700

    added params to fetch: conjunction, sort, where, search, etc fix #16, fix #18

--- R/enigma_fetch.r
+++ R/enigma_fetch.r
@@ -3,6 +3,19 @@
 #' @export
 #'
 #' @param dataset Dataset name. Required.
+#' @param select (character) Vector of columns to be returned with each row. Default is to return
+#' all columns.
+#' @param conjunction one of "and" or "or". Only applicable when more than one \code{search}
+#' or \code{where} parameter is provided. Default: "and"
+#' @param sort (character) Sort rows by a particular column in a given direction. + denotes
+#' ascending order, - denotes descending. See examples.
+#' @param where (character) Filter results with a SQL-style "where" clause. Only applies to
+#' numerical columns - use the \code{search} parameter for strings. Valid operators are >, < and =.
+#' Only one \code{where} clause per request is currently supported.
+#' @param search (character) Filter results by only returning rows that match a search query. By
+#' default this searches the entire table for matching text. To search particular fields only, use
+#' the query format "@@fieldname query". To match multiple queries, the | (or) operator can be used
+#' eg. "query1|query2".
 #' @param path File name and path of output zip file. Defaults to write a zip file to your home
 #' directory with name of the dataset, and file extension \code{.csv.gz}.
 #' @param overwrite Will only overwrite existing path if TRUE.
@@ -24,7 +37,31 @@
 #'
 #' # Fetch the Crunchbase companies info dataset
 #' res <- enigma_fetch(dataset='com.crunchbase.info.companies.acquisition')
-#' enigma_read(res)
+#' head(enigma_read(res))
+#' 
+#' # Use the select parameter to limit fields returned
+#' res <- enigma_fetch(dataset='com.crunchbase.info.companies.acquisition', 
+#'    select = c("acquisition", "price_amount", "acquired_year"))
+#' head(enigma_read(res))
+#' 
+#' # Use the search parameter to query entire table or particular fields
+#' res <- enigma_fetch(dataset='com.crunchbase.info.companies.acquisition', 
+#'    search = "holdings")
+#' head(enigma_read(res))
+#' 
+#' # Use the search parameter to query entire table or particular fields
+#' res <- enigma_fetch(dataset='com.crunchbase.info.companies.acquisition', 
+#'    where = "price_amount > 0", select = c("acquisition", "price_amount"))
+#' df <- enigma_read(res)
+#' options(scipen = 99)
+#' head(df) 
+#' 
+#' # Use the sort parameter to sort results
+#' res <- enigma_fetch(dataset='com.crunchbase.info.companies.acquisition', 
+#'    sort = "-price_amount", select = c("acquisition", "price_amount"))
+#' df <- enigma_read(res)
+#' options(scipen = 99)
+#' head(df) 
 #'
 #' # Piping workflow
 #' library('dplyr')
@@ -37,9 +74,16 @@
 #' enigma_fetch(dataset='com.crunchbase.info.companies.acquisition', config=verbose())
 #' }
 
-enigma_fetch <- function(dataset=NULL, path=NULL, overwrite = TRUE, key=NULL, ...) {
+enigma_fetch <- function(dataset = NULL, select = NULL, search = NULL, where = NULL, 
+  conjunction = NULL, sort = NULL, path=NULL, overwrite = TRUE, key=NULL, ...) {
+  
   url <- sprintf('%s/export/%s/%s', en_base(), check_key(key), check_dataset(dataset))
-  res <- GET(url, ...)
+  sw <- proc_search_where(search, where)
+  if (!is.null(select)) select <- paste(select, collapse = ",")
+  args <- list(select = select, conjunction = conjunction, sort = sort)
+  args <- as.list(unlist(ec(c(sw, args))))
+  if (length(args) == 0) args <- NULL
+  res <- GET(url, query = args, ...)
   json <- error_handler(res)
   if (is.null(path)) path <- file.path(Sys.getenv('HOME'), parse_url(json$export_url)$path)
   bin <- GET(json$export_url, write_disk(path = path, overwrite = overwrite), ...)
