5e671a52641fdecc9307010307c206a6baceb092
Scott Chamberlain <myrmecocystus@gmail.com>
2017-02-13 09:52:45 -0800

    fix #20 users can use env var or option var

--- man-roxygen/key-curl.R
+++ man-roxygen/key-curl.R
@@ -1,6 +1,7 @@
 #' @param key (character) Required. An Enigma API key. Supply in the function 
-#' call, or store in your \code{.Rprofile} file, or do 
-#' \code{options(enigmaKey = "<your key>")}. Obtain an API key by creating 
+#' call, or store in your \code{.Renviron} file like 
+#' \code{ENIGMA_KEY=your key)}, or in your \code{.Rprofile} file as 
+#' \code{options(enigmaKey = "<your key>")}, Obtain an API key by creating 
 #' an account with Enigma at \url{http://enigma.io}, then obtain an API key 
 #' from your account page.
 #' @param ... Named curl options passed on to \code{\link[crul]{HttpClient}}
