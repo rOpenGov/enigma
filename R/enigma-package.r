853dec934be828a9eecfd84c716c84cdfacef0e9
Scott Chamberlain <myrmecocystus@gmail.com>
2018-02-15 08:42:10 -0800

    changes

--- R/enigma-package.r
+++ R/enigma-package.r
@@ -24,7 +24,7 @@
 #' function call, or store in your \code{.Renviron} file like 
 #' \code{ENIGMA_KEY=your key)}, or in your \code{.Rprofile} file as 
 #' \code{options(enigmaKey = "<your key>")}, Obtain an API key by creating 
-#' an account with Enigma at \url{http://enigma.io}, then obtain an API key 
+#' an account with Enigma at \url{https://www.enigma.com}, then obtain an API key 
 #' from your account page.
 #'
 #' @importFrom plyr rbind.fill
